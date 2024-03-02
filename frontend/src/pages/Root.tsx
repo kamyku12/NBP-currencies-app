5038812c9154f843f8ea9524d1057fbcb07e235b
Jakub Kaminski <kubakam8@gmail.com>
2024-03-02 11:46:45 +0100

    feat: added new components for balance page

--- frontend/src/pages/Root.tsx
+++ frontend/src/pages/Root.tsx
@@ -1,5 +1,23 @@
+import UserAssets from "@components/User/UserAssets"
+import { Typography } from "@mui/material"
+import { Stack } from "@mui/system"
+
 function Root() {
-    return <p>Hello World</p>
+    // TODO: change user to value from context 
+    return <Stack>
+        <Typography variant='h2'>
+            Hello user
+        </Typography>
+        {/* TODO: change value to value from context */}
+        <Typography mb='1rem'>
+            Your portfolio balance is: balance
+        </Typography>
+        <Typography>
+            Assets:
+        </Typography>
+        <UserAssets />
+
+    </Stack>
 }
 
 export default Root
